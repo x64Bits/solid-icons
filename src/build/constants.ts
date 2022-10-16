b863eccdf25dc58bc60277b11fd33adb62d6545f
Ignacio Zsabo <ignacioluisr2006@gmail.com>
2022-10-16 16:23:31 -0400

    feat: all icons package

--- src/build/constants.ts
+++ src/build/constants.ts
@@ -10,7 +10,7 @@ export const WEB_PATH = path.resolve(
   ROOT_PATH,
   process.env.WEB_PATH || DEFAULT_WEB_PATH
 );
-export const WEB_ICONS_PATH = path.resolve(WEB_PATH, "icons");
+export const WEB_ICONS_PATH = path.resolve(WEB_PATH);
 export const log = console.log;
 
 export const LIB_PACKAGE_JSON = /* javascript */ `{
