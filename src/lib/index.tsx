017fbd8acf10e61d81318b95dc77fe04ed0a36af
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-02-01 00:21:59 -0800

    fix: remove unused types from the lib

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -3,20 +3,11 @@ export { isServer, mergeProps } from "solid-js/web";
 
 type SVGSVGElementTags = JSX.SVGElementTags["svg"];
 
-export interface IconTree {
-  a: SVGSVGElementTags;
-  c: string;
-}
-
 export interface IconProps extends SVGSVGElementTags {
   size?: string | number;
   color?: string;
   title?: string;
   style?: JSX.CSSProperties;
-}
-
-export interface IconBaseProps extends IconProps {
-  src: IconTree;
 }
 
 export declare type IconTypes = (props: IconProps) => JSX.Element;
