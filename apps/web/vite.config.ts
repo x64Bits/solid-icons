1643c1842eee80268022b92fe57369cfad7c6b9f
Ignacio Zsabo <ignacioluisr2006@gmail.com>
2026-01-20 17:14:04 -0400

    feat: Refactor Vite configuration for improved alias handling and update package entry points

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,19 +2,29 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import path from "path";
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: "esnext",
-  },
-  resolve: {
-    alias: {
-      "solid-icons": path.resolve(__dirname, "../../packages/solid-icons/dist"),
-      "@public": "public",
-      "~": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const aliases = {
+    "@public": "public",
+    "~": path.resolve(__dirname, "./src"),
+  };
+
+  if (mode === "development") {
+    aliases["solid-icons"] = path.resolve(
+      __dirname,
+      "../../packages/solid-icons/dist"
+    );
+  }
+
+  return {
+    plugins: [solidPlugin()],
+    server: {
+      port: 3000,
     },
-  },
+    build: {
+      target: "esnext",
+    },
+    resolve: {
+      alias: aliases,
+    },
+  };
 });
