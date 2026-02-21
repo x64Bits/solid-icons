7b7eda9da6f6a3377de9cf55a8f0925cb4deaea3
elgabrielcisneros <tilordqwertydev3@gmail.com>
2026-02-21 18:44:42 -0400

    Use icons in nav header 🎨

--- apps/tanstack-test/src/routes/__root.tsx
+++ apps/tanstack-test/src/routes/__root.tsx
@@ -4,52 +4,58 @@ import {
   Link,
   Scripts,
   createRootRoute,
-} from '@tanstack/solid-router';
-import { TanStackRouterDevtools } from '@tanstack/solid-router-devtools';
-import { HydrationScript } from 'solid-js/web';
-import type * as Solid from 'solid-js';
-import { DefaultCatchBoundary } from '~/components/DefaultCatchBoundary';
-import { NotFound } from '~/components/NotFound';
-import appCss from '~/styles/app.css?url';
-import { seo } from '~/utils/seo';
+} from "@tanstack/solid-router";
+import { TanStackRouterDevtools } from "@tanstack/solid-router-devtools";
+import { HydrationScript } from "solid-js/web";
+import type * as Solid from "solid-js";
+import { DefaultCatchBoundary } from "~/components/DefaultCatchBoundary";
+import { NotFound } from "~/components/NotFound";
+import appCss from "~/styles/app.css?url";
+import { seo } from "~/utils/seo";
+
+// Icons
+
+import { FiAtSign, FiCommand, FiHome, FiUsers } from "solid-icons/fi";
+import { IoMapOutline } from "solid-icons/io";
+import { MdFillNot_listed_location } from "solid-icons/md";
 
 export const Route = createRootRoute({
   head: () => ({
     meta: [
       {
-        charset: 'utf-8',
+        charset: "utf-8",
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
       },
       ...seo({
         title:
-          'TanStack Start | Type-Safe, Client-First, Full-Stack React Framework',
+          "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
         description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
       }),
     ],
     links: [
-      { rel: 'stylesheet', href: appCss },
+      { rel: "stylesheet", href: appCss },
       {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png',
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
       },
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png',
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
       },
       {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png',
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
       },
-      { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
-      { rel: 'icon', href: '/favicon.ico' },
+      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
+      { rel: "icon", href: "/favicon.ico" },
     ],
   }),
   errorComponent: DefaultCatchBoundary,
@@ -65,56 +71,62 @@ function RootDocument({ children }: { children: Solid.JSX.Element }) {
       </head>
       <body>
         <HeadContent />
-        <div class="p-2 flex gap-2 text-lg">
+        <div class="p-2 flex gap-2 text-xl gap-2 items-center">
           <Link
             to="/"
             activeProps={{
-              class: 'font-bold',
+              class: "font-bold",
             }}
             activeOptions={{ exact: true }}
           >
             Home
-          </Link>{' '}
+            <FiHome />
+          </Link>{" "}
           <Link
             to="/posts"
             activeProps={{
-              class: 'font-bold',
+              class: "font-bold",
             }}
           >
             Posts
-          </Link>{' '}
+            <FiCommand />
+          </Link>{" "}
           <Link
             to="/users"
             activeProps={{
-              class: 'font-bold',
+              class: "font-bold",
             }}
           >
             Users
-          </Link>{' '}
+            <FiUsers />
+          </Link>{" "}
           <Link
             to="/route-a"
             activeProps={{
-              class: 'font-bold',
+              class: "font-bold",
             }}
           >
-            Pathless Layout
-          </Link>{' '}
+            Layout
+            <IoMapOutline />
+          </Link>{" "}
           <Link
             to="/deferred"
             activeProps={{
-              class: 'font-bold',
+              class: "font-bold",
             }}
           >
             Deferred
-          </Link>{' '}
+            <FiAtSign />
+          </Link>{" "}
           <Link
             // @ts-expect-error
             to="/this-route-does-not-exist"
             activeProps={{
-              class: 'font-bold',
+              class: "font-bold",
             }}
           >
-            This Route Does Not Exist
+            Not found
+            <MdFillNot_listed_location />
           </Link>
         </div>
         <hr />
--- apps/tanstack-test/src/routes/index.tsx
+++ apps/tanstack-test/src/routes/index.tsx
@@ -10,24 +10,18 @@ function Home() {
   return (
     <div class="p-2 flex flex-col gap-2 items-center">
       <div class="flex items-center gap-2">
-        <IoHomeOutline
-          class="text-2xl font-bold text-white"
-          size={30}
-          color="#ffffffff"
-        />
+        <IoHomeOutline class="text-2xl font-bold" size={30} color="#ffffffff" />
         <h1 class="text-2xl font-bold text-white">
           Welcome Home!!! This is a test
         </h1>
       </div>
       <div class="flex items-center gap-2">
         <HiOutlineArchiveBox
-          class="text-2xl font-bold text-white"
+          class="text-2xl font-bold"
           size={30}
           color="#ffffffff"
         />
-        <h1 class="text-2xl font-bold text-white">
-          Welcome Box!!! This is a test
-        </h1>
+        <h1 class="text-2xl font-bold">Welcome Box!!! This is a test</h1>
       </div>
     </div>
   );
